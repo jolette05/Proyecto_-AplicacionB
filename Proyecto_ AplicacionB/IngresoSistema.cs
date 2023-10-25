f5a15f55397eda124b65f144e7f103f1846db924
jolette05 <america22506@cbtis75.edu.mx>
2023-10-24 23:38:32 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_ AplicacionB/IngresoSistema.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto__AplicacionB
+{
+    public partial class sistema : Form
+    {
+        public sistema()
+        {
+            InitializeComponent();
+        }
+    }
+}
